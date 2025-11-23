23cdf07566d89fa2e4baa6ef040c0a821f426b69
elverarroyave-udea <arroyave.elver2@gmail.com>
2025-11-22 22:54:10 -0500

    Configuracion tablas para obtener letra de columna

---
+++ tables.js
@@ -0,0 +1,82 @@
+INGRESOS_EGRESOS={
+    FECHA:'A',
+    REFERENCIA:'B',
+    NUM:'C',
+    CODIGO:'D',
+    DETALLE:'E',
+    ENTRADA:'G',
+    M_PAGO:'H',
+}
+
+PAGO_PEDIDOS={
+    FECHA:'A',
+    CODIGO:'B',
+    COD_IN_OUT:'C',
+    COD_IN:'D',
+    PRODUCTO:'E',
+    METODO_DE_COMPRA:'F',
+    VALOR:'G',
+}
+
+ABONOS={
+    FECHA:'A',
+    CODIGO:'B',
+    COD_IN_OUT:'C',
+    COD_VENTA:'D',
+    CLIENTE:'E',
+    PRODUCTO:'F',
+    VALOR:'G',
+}
+
+CONTROL_VENTAS={
+    FECHA:'A',
+    NUM:'B',
+    CODIGO:'C',
+    SOCIO:'D',
+    C_PAGOS:'E',
+    COD_PRDT: 'F',
+    CANTIDAD: 'H',
+    COD_CLNT: 'I',
+    PRECIO: 'L',
+    M_PAGO: 'M',
+    PAGO: 'N'
+}
+
+INVENTARIO={
+    NOMBRE: 'A',
+    NUM:'B',
+    CODIGO:'C',
+    CATEGORIA:'D',
+    UNO:'E'
+}
+
+CLIENTES={
+    NOMBRE: 'A',
+    NUM:'B',
+    CODIGO:'C',
+    DOCUMENTO:'D',
+    TELEFONO:'E',
+    CORREO:'F',
+    DIRECCION:'G',
+    SOCIO:'H'
+}
+
+CONTROL_INGRESO={
+    FECHA:'A',
+    NUM_IN:'B',
+    COD_INGRESO:'C',
+    QUIEN_RECIBE:'D',
+    COD_PROV:'E',
+    PROVEEDOR:'F',
+    COD_PRDT:'G',
+    PRODUCTO:'H',
+    CANTIDAD:'I',
+    PRECIO_UNITARIO:'J',
+    METODO_DE_PAGO:'K',
+    SALDO_PAGADO:'L',
+    TOTAL:'M',
+    PAGADO:'N',
+    ESTADO:'O',
+    ENLACE_FACTURA:'P'
+}
+
